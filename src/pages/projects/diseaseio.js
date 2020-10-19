d912f0743e8be60eff2eca4eccff7ca0b5b92f31
Jeremy Chui <jeremy@jchui.me>
2020-10-19 22:57:27 +0100

    Fixed diseaseio

---
+++ src/pages/projects/diseaseio.js
@@ -0,0 +1,42 @@
+import React from "react"
+import cover from "../projects/img/disease-cover.png"
+
+import Layout from "../../components/layout"
+import SEO from "../../components/seo"
+
+const ProjectPage = () => (
+  <Layout>
+    <SEO 
+      title="Disease.io" 
+      description="A designer, developer, and doctor based in 
+                  the United Kingdom. Passionate about 
+                  innovation in medicine & technology."
+      author="@jtkchui"
+      />
+      
+      <div className="container">
+          <div className="columns">
+            <div className="column">
+                <h2>Disease.io</h2>
+                <p>
+                    <small>Founder & Technical Lead</small>
+                </p>
+            </div>
+            <div className="column">
+              <a href="https://disease.io" className="download float-right">Visit Project</a>
+            </div>
+          </div>
+
+          <div className="columns">
+            <div className="column">
+            <img src={cover} />
+            <p>This page is currently under construction. For more information about this project, please check <a href="https://disease.io">disease.io</a>.</p>
+            </div>
+          </div>
+          
+      </div>
+
+  </Layout>
+)
+
+export default ProjectPage
