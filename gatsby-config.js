67d409f85273d18e62de5727699efa4a70359d19
Jeremy Chui <jeremy@jchui.me>
2020-10-20 00:02:00 +0100

    Added Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,5 +31,11 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-gtag`,
+      options: {
+        trackingId: `UA-24142610-6`,
+      },
+    },
   ],
 }
