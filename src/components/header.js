73419cb7b24653baf6c0f077ea49470403664992
Jeremy Chui <jeremy@jchui.me>
2020-10-19 13:21:46 +0100

    Added navbar

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,28 @@
 import { Link } from "gatsby"
-import PropTypes from "prop-types"
 import React from "react"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+const Header = () => (
+  <header>
+    <div className="container">
+      <div className="columns">
+        <div className="column">
+          <Link to="../">
+            <p className="logo">
+              <strong>Jeremy </strong>
+              Chui
+            </p>
+          </Link>
+        </div>
+        <div className="column">
+          <ul>
+            <li><Link to="">Projects</Link></li>
+            <li><Link to="">About</Link></li>
+            <li><Link to="">Résumé</Link></li>
+          </ul>
+        </div>
+      </div>
     </div>
   </header>
 )
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
-}
 
 export default Header
